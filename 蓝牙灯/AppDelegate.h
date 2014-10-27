e37d47de41a10ab14b06291108ea3514bedd9119
tuner168-ios <tuner168-ios@tuner168-iosdeMac-mini.local>
2014-10-27 11:18:53 +0800

    Initial Commit

---
+++ 蓝牙灯/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  蓝牙灯
+//
+//  Created by tuner168-ios on 14-10-27.
+//  Copyright (c) 2014年 _wangli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ 蓝牙灯/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  蓝牙灯
+//
+//  Created by tuner168-ios on 14-10-27.
+//  Copyright (c) 2014年 _wangli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
